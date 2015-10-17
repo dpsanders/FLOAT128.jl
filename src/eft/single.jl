4478919e4b66789c80c32e85b9c144bb05838249
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2015-10-17 14:53:05 -0400

    Create single.jl
    Error-Free Transformations of a single Float64.

---
+++ src/eft/single.jl
@@ -0,0 +1,23 @@
+@inline function eftSquare(a::Float64)
+    x = a * a
+    y = fma(a, a, -x)
+    x,y
+end
+
+function eftCube(a::Float64)
+    p = a*a; e = fma(a, a, -p)
+    x = p*a; p = fma(p, a, -x)
+    y = e*a
+    x,y
+end
+
+# !!sassafrass!!
+# presently 'y' must be negated to get the right result
+# I do not know why.
+#
+@inline function eftRecip(a::Float64)
+     x = one(Float64)/a
+     y = fma(x,a,-1.0)
+     y = (y!=zero(Float64)) ? -y/a : y
+     x,y
+end
