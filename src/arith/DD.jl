16aa0ee351edf9d9486711d50275d7509d683f18
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2015-10-24 02:46:50 -0400

    Update DD.jl

--- src/arith/DD.jl
+++ src/arith/DD.jl
@@ -146,25 +146,34 @@ end
 
 
 # roots
-#=
+
 function sqrt(a::DD)
     if a.hi <= zero(Float64)
        if a.hi == zero(Float64)
            return zero(DD)
        else
-           throw(DomainError("sqrt expects a nonnegative base"))
+           throw(ArgumentError("sqrt expects a nonnegative base"))
        end
+    elseif (a.hi < 1.0e-18) | (a.hi > 1.0e18)
+        throw(ArgumentError("sqrt arg ($a) outside domain"))
     end
 
-    r = one(Float64)/sqrt(a.hi)
+    if (a.hi < 1.0e-7)  # -log2(1.0e-7) < (1/2) Float64 significand bits
+        return one(DD) / sqrt(one(DD)/a)
+    end
 
-    r += (r * (one(Float64) - (a * (r*r)))) * 0.5
-    r += (r * (one(Float64) - (a * (r*r)))) * 0.5
-    r += (r * (one(Float64) - (a * (r*r)))) * 0.5
+    # initial approximation to 1/sqrt(a)
+    r = DD(one(Float64)/sqrt(a.hi), zero(Float64))
 
-    r*a
+    r = r + divby2( r * (one(DD) - (a*(r*r))) )
+    r = r + divby2( r * (one(DD) - (a*(r*r))) )
+    r = r + divby2( r * (one(DD) - (a*(r*r))) )
+
+    r = a*r
+    divby2(r + a/r)
 end
-=#
+
+
 
 
 #=
