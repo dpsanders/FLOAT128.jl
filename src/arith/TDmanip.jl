142ff836f3bb79f14f92cee21a75a3abda77aecc
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2015-10-27 18:01:41 -0400

    Update TDmanip.jl

--- src/arith/TDmanip.jl
+++ src/arith/TDmanip.jl
@@ -12,16 +12,24 @@ signbit(a::TD) = signbit(a.hi)
 (-)(a::TD) = TD(-a.hi,-a.md,-a.lo)
 (abs)(a::TD) = (a.hi >= zero(Float64) ? a : -a)
 
-flipsign(a::TD,b::TD) = TD(flipsign(a.hi,b.hi), flipsign(a.md,b.hi), flipsign(a.lo,b.hi))
-flipsign(a::TD,b::DD) = TD(flipsign(a.hi,b.hi), flipsign(a.md,b.hi), flipsign(a.lo,b.hi))
+
+@inline flipsign(a::TD,b::TD) = TD(flipsign(a.hi,b.hi), flipsign(a.md,b.hi), flipsign(a.lo,b.hi))
+@inline flipsign(a::TD,b::DD) = TD(flipsign(a.hi,b.hi), flipsign(a.md,b.hi), flipsign(a.lo,b.hi))
 flipsign(a::DD,b::TD) = DD(flipsign(a.hi,b.hi), flipsign(a.lo,b.hi))
 flipsign(a::TD,b::Float64) = TD(flipsign(a.hi,b), flipsign(a.md,b.hi), flipsign(a.lo,b))
 flipsign(a::TD,b::Integer) = TD(flipsign(a.hi,b), flipsign(a.md,b.hi), flipsign(a.lo,b))
-copysign(a::TD,b::TD) = TD(copysign(a.hi,b.hi), flipsign(a.md,b.hi), flipsign(a.lo,b.hi))
-copysign(a::TD,b::DD) = TD(copysign(a.hi,b.hi), flipsign(a.md,b.hi), flipsign(a.lo,b.hi))
-copysign(a::DD,b::TD) = DD(copysign(a.hi,b.hi), flipsign(a.lo,b.hi))
-copysign(a::TD,b::Float64) = TD(copysign(a.hi,b), flipsign(a.md,b.hi), flipsign(a.lo,b))
-copysign(a::TD,b::Integer) = TD(copysign(a.hi,b), flipsign(a.md,b.hi), flipsign(a.lo,b))
+
+@inline function copysign(a::TD,b::Float64)
+    if (b < zero(Float64))
+        a.hi < zero(Float64) ? a : -a
+    else
+        a.hi < zero(Float64) ? -a : a
+end
+copysign(a::TD,b::Integer) = copysign(a,convert(Float64,b))
+copysign(a::TD,b::DD) = copysign(a,b.hi)
+@inline copysign(a::TD,b::TD) = copysign(a, b.hi)
+copysign(a::DD,b::TD) = copysign(a.hi,b.hi)
+
 
 @inline function mulby2(a::TD)
     TD(a.hi*2.0, a.md*2.0, a.lo*2.0)
