2420cb6975679845a73993c8dc9acabc153808f3
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2015-10-27 14:47:11 -0400

    rework

--- src/trig/DD.jl
+++ src/trig/DD.jl
@@ -1,4 +1,140 @@
-function sin_taylor(a::DD)
+@inline function mod2piAsTD(a::DD)
+    b = TD(a)
+    b1 = b * td_1_over_twopi
+    f1 = floor(b1)
+    chk = floor(b.hi * td_1_over_twopi_part2.hi)
+    if (chk > zero(Float64))
+       throw(ErrorException("arg ($a) is too large"))
+    end
+    d1 = f1*td_twopi
+    d2 = f1*td_twopi_part2
+  # d3 = f1*td_twopi_part3
+
+    m = b - d1
+    m = m - d2
+  # m = m - d3
+
+    m
+end
+
+function mod2pi(a::DD)
+    DD(mod2piAsTD(a))
+end
+
+@inline function modPiOver2AsTD(a::DD)
+    b = TD(a)
+    b1 = b * td_2_over_pi
+    f1 = floor(b1)
+    chk = floor(b.hi * td_2_over_pi_part2.hi)
+    if (chk > zero(Float64))
+       throw(ErrorException("arg ($a) is too large"))
+    end
+    d1 = f1*td_pi_over_2
+    d2 = f1*td_pi_over_2_part2
+    # d3 = f1*td_pi_over_2_part3
+
+    m = b - d1
+    m = m - d2
+    # m = m - d3
+
+    m
+end
+
+# a --> [0 .. p/2)
+function modPiOver2(a::DD)
+    DD(modPiOver2AsTD(a))
+end
+
+
+@inline function modPiOver4AsTD(a::DD)
+    b = TD(a)
+    b1 = b * td_4_over_pi
+    f1 = floor(b1)
+    chk = floor(b.hi * td_4_over_pi_part2.hi)
+    if (chk > zero(Float64))
+       throw(ErrorException("arg ($a) is too large"))
+    end
+    d1 = f1*td_pi_over_4
+    d2 = f1*td_pi_over_4_part2
+    # d3 = f1*td_pi_over_4_part3
+
+    m = b - d1
+    m = m - d2
+    # m = m - d3
+
+    m
+end
+
+# a --> [0 .. p/2)
+function modPiOver4(a::DD)
+    DD(modPiOver4AsTD(a))
+end
+
+# a --> [-pi/4 .. p/4)
+function modSignedPiOver4(a::DD)
+    m = modPiOver2AsTD(a)
+    if (m.hi >= pi/4.0)
+        m = m - td_pi_over_2
+        m = m - td_pi_over_2_part2
+        # m = m - td_pi_over_2_part3
+    end
+
+    DD(m)
+end
+#=
+more accurate
+=#
+
+function modSignedPiOver4(a::DD)
+    m = modPiOver2AsTD(a)
+    if (m.hi >= pi/4.0)
+        m = m - td_pi_over_2
+        m = m - td_pi_over_2_part2
+        # m = m - td_pi_over_2_part3
+    end
+
+    DD(m)
+end
+
+
+# -2pi <= radian < 2pi
+function resolveQuadrantInCircle(radian::DD)
+    isneg, aradian = signbit(radian), abs(radian)
+    if isneg
+        aradian = DD(td_twopi - aradian)
+    end
+    # octant numbering for trig calcs are rotated by -pi/4
+    # (see, e.g. Jack Crenshaw, Math Toolkit for Realtime Programming, p106)
+    octant = trunc(Int, div(radian, dd_pi_over_4).hi)
+    quadrant = (octant + isodd(octant)) >> 1
+    quadrant = mod(quadrant,4)
+    radi = modSignedPiOver4(aradian)
+    quadrant, radi
+end
+
+
+const dd_n_tan_coeff = 16;
+const dd_tan_coeff = DD[
+   DD(1.0, 0.0),
+   DD(0.3333333333333333, 1.850371707708594e-17),
+   DD(0.13333333333333333, 1.8503717077085942e-18),
+   DD(0.05396825396825397, -2.5552752154071065e-18),
+   DD(0.021869488536155203, -1.7377829530067485e-19),
+   DD(0.008863235529902197, -7.63300580171831e-19),
+   DD(0.003592128036572481, -1.253823608406629e-19),
+   DD(0.0014558343870513183, -6.214492640136062e-20),
+   DD(0.000590027440945586, 3.478690842383652e-20),
+   DD(0.00023912911424355248, 3.564613898329782e-21),
+   DD(9.691537956929451e-5, -6.2386628755632464e-21),
+   DD(3.927832388331683e-5, 1.3737015743076767e-21),
+   DD(1.5918905069328964e-5, 1.0427554807190543e-21),
+   DD(6.451689215655431e-6, 1.1519922496640058e-22),
+   DD(2.6147711512907546e-6, -9.313685621299801e-23),
+   DD(1.0597268320104654e-6, 2.3670525505213632e-24), # 31
+];
+
+
+function sin_taylor_series(a::DD)
   x = a
   x2 = x*x
   x3 = x*x2
@@ -19,7 +155,7 @@ function sin_taylor(a::DD)
   DD(z + ((z4+z3)+z2))
 end
 
-function cos_taylor(a::DD)
+function cos_taylor_series(a::DD)
   x = a
   x2 = x*x
   x4 = x2*x2
@@ -37,56 +173,10 @@ function cos_taylor(a::DD)
 end
 
 
-function sincos_taylor(a::DD)
-    s = c = zero(DD)
-    if abs(a.hi) < 0.39269908169872414 # pi/8
-        s = sin_taylor(a)
-        c = sqrt(1.0-s*s)
-    else
-        c = cos_taylor(a)
-        s = sqrt(1.0-c*c)
-    end
-    s,c
-end
-
-const dd_n_tan_coeff = 31;
-const dd_tan_coeff = DD[
-   DD(1.0, 0.0),
-   DD(0.9, 0.0),
-   DD(0.3333333333333333, 1.850371707708594e-17),
-   DD(0.0, 0.0),
-   DD(0.13333333333333333, 1.8503717077085942e-18),
-   DD(0.0, 0.0),
-   DD(0.05396825396825397, -2.5552752154071065e-18),
-   DD(0.0, 0.0),
-   DD(0.021869488536155203, -1.7377829530067485e-19),
-   DD(0.0, 0.0),
-   DD(0.008863235529902197, -7.63300580171831e-19),
-   DD(0.0, 0.0),
-   DD(0.003592128036572481, -1.253823608406629e-19),
-   DD(0.0, 0.0),
-   DD(0.0014558343870513183, -6.214492640136062e-20),
-   DD(0.0, 0.0),
-   DD(0.000590027440945586, 3.478690842383652e-20),
-   DD(0.0, 0.0),
-   DD(0.00023912911424355248, 3.564613898329782e-21),
-   DD(0.0, 0.0),
-   DD(9.691537956929451e-5, -6.2386628755632464e-21),
-   DD(0.0, 0.0),
-   DD(3.927832388331683e-5, 1.3737015743076767e-21),
-   DD(0.0, 0.0),
-   DD(1.5918905069328964e-5, 1.0427554807190543e-21),
-   DD(0.0, 0.0),
-   DD(6.451689215655431e-6, 1.1519922496640058e-22),
-   DD(0.0, 0.0),
-   DD(2.6147711512907546e-6, -9.313685621299801e-23),
-   DD(0.0, 0.0),
-   DD(1.0597268320104654e-6, 2.3670525505213632e-24), # 31
-];
 
 # for a < 9/64 0.140625
-function tan_taylor_series(a::DD)
-  x = a
+function tan_taylor_series(radian::DD)
+  x = radian
   x2 = x*x
   x3 = x*x2
   x4 = x2*x2
@@ -98,79 +188,199 @@ function tan_taylor_series(a::DD)
   x17 = x8*x9
   x25 = x17*x8
 
-  z = TD(x) + x*(dd_tan_coeff[3]*x2 + dd_tan_coeff[5]*x4 + dd_tan_coeff[7]*x6)
-  z2 = x9 * (dd_tan_coeff[9] + x2*dd_tan_coeff[11] + x4*dd_tan_coeff[13] + x6*dd_tan_coeff[15])
-  z3 = x17 * (dd_tan_coeff[17] + x2*dd_tan_coeff[19] + x4*dd_tan_coeff[21] + x6*dd_tan_coeff[23])
-  z4 = x25 * (dd_tan_coeff[25] + x2*dd_tan_coeff[27] + x4*dd_tan_coeff[29] + x6*dd_tan_coeff[31])
+  z = TD(x) + x*(dd_tan_coeff[2]*x2 + dd_tan_coeff[3]*x4 + dd_tan_coeff[4]*x6)
+  z2 = x9 * (dd_tan_coeff[5] + x2*dd_tan_coeff[6] + x4*dd_tan_coeff[7] + x6*dd_tan_coeff[8])
+  z3 = x17 * (dd_tan_coeff[9] + x2*dd_tan_coeff[10] + x4*dd_tan_coeff[11] + x6*dd_tan_coeff[12])
+  z4 = x25 * (dd_tan_coeff[13] + x2*dd_tan_coeff[14] + x4*dd_tan_coeff[15] + x6*dd_tan_coeff[16])
 
   DD(z + ((z4+z3)+z2))
 end
 
-# a <= 0.15
-function tan_taylor(a::DD)
-    if (a.hi <= 9.0/64.0)
-        tan_taylor_series(a)
-    else
-        s,c = sincos_taylor(a)
-        s/c
-    end
-end
 
-function sinpio2(a::DD)
-    if a.hi == zero(Float64)
-        return zero(DD)
-    end
-    if (a >= dd_pi_over_4)
-        b = dd_pi_over_2 - a
-        if (b.hi == zero(Float64))
-           one(DD)
-        else
-           cos_taylor(b)
+
+function sin_taylor(radian::DD)
+    hi = radian.hi
+    if ((hi==zero(Float64)) | (hi==NaN) | (hi==pi/4.0) | (hi==pi/6.0))
+        if hi == zero(Float64)
+            return zero(DD)
+        elseif hi == NaN
+            return radian
+        elseif hi == dd_pi_over_4.hi && radian.lo == dd_pi_over_4.lo
+            return dd_1_over_sqrt2
+        elseif hi == dd_pi_over_6.hi && radian.lo == dd_pi_over_6.lo
+            return dd_half
         end
+    end
+    sin_taylor_series(radian)
+end
+
+function cos_taylor(radian::DD)
+    hi = radian.hi
+    if ((hi==zero(Float64)) | (hi==NaN) | (hi==pi/4.0) | (hi==pi/6.0))
+        if hi == zero(Float64)
+            return one(DD)
+        elseif hi == NaN
+            return radian
+        elseif hi == dd_pi_over_4.hi && radian.lo == dd_pi_over_4.lo
+            return dd_1_over_sqrt2
+        elseif hi == dd_pi_over_6.hi && radian.lo == dd_pi_over_6.lo
+            return dd_sqrt3_over_2
+        end
+    end
+    cos_taylor_series(radian)
+end
+
+
+function sincos_taylor(radian::DD)
+    s = c = zero(DD)
+    if abs(radian.hi) < 0.39269908169872414 # pi/8
+        s = sin_taylor(radian)
+        ts = TD(s)
+        c = DD(sqrt(one(TD)-ts*ts))
     else
-        sin_taylor(a)
+        c = cos_taylor(radian)
+        tc = TD(c)
+        s = DD(sqrt(one(TD)-tc*tc))
     end
+    s,c
 end
 
-function sin02pi(a::DD)
-    @assert(a >= zero(DD))
-    sgnbit = false
-    if (a.hi == zero(Float64)) | (a == dd_twopi)
-        return zero(DD)
-    end
-    b = a
-    if a >= dd_pi
-        sgnbit = !sgnbit
-        a = dd_twopi - a
-    end
-    # a in [0, pi)
-    if a >= dd_pi_over_2
-        a = dd_pi - a
-    end
-    # a in [0,pi/2)
-    @assert(a >= zero(DD))
-    @assert(a <= dd_pi_over_2)
-    s = sinpio2(a)
-    sgnbit ? -s : s
-end
 
-# !!make it better and put it in arith/DDsupp.jl!!
-function mod2pi(a::DD)
-    mod(a,dd_twopi)
-end
-
-function sin(a::DD)
-    sgnbit, aa = signbit(a),abs(a)
-
-    if aa.hi == zero(Float64)
-        return aa
+# a <= 0.15
+function tan_taylor(radian::DD)
+    hi = radian.hi
+    if ((hi==zero(Float64)) | (hi==NaN) | (hi==pi/4.0) | (hi==pi/6.0))
+        if hi == zero(Float64)
+            return zero(DD)
+        elseif hi == NaN
+            return radian
+        elseif hi == dd_pi_over_4.hi && radian.lo == dd_pi_over_4.lo
+            return one(DD)
+        elseif hi == dd_pi_over_6.hi && radian.lo == dd_pi_over_6.lo
+            return dd_sqrt3_over_3
+        end
     end
-    if aa < dd_pi_over_4
-        aa = sin_taylor(aa)
-    elseif aa >= dd_twopi
-        aa = sin02pi( mod2pi(aa) )
+    isneg, aradian = signbit(radian), abs(radian)
+    if (1.0e-15 <= aradian.hi <= 9.0/64.0)
+        t = tan_taylor_series(aradian)
     else
-        aa = sin02pi(aa)
+        s,c = sincos_taylor(aradian)
+        t = s/c
     end
-    sgnbit ? -aa : aa
+    isneg ? -t : t
+end
+
+
+
+
+#=
+   for radian in [0,8*pi)
+      relerr precise sin(radian) ~1.2e-32 (106 bits)
+   for radian in [0,4096^3 *pi)
+      relerr precise sin(radian) ~2e-32 (105 bits)
+   for radian > 4096^3 *pi
+      relerr 1.5e-29 (95 bits)
+=#
+
+function sinInCircle(radian::DD)
+  quadrant, radi = resolveQuadrantInCircle(radian)
+  isneg, aradi = signbit(radi), abs(radi)
+  if (quadrant == 0)
+    isneg ? -sin_taylor(aradi) : sin_taylor(aradi)
+  elseif (quadrant == 1)
+    cos_taylor(aradi)
+  elseif (quadrant == 2)
+    isneg ? sin_taylor(aradi) : -sin_taylor(aradi)
+  else #if (quadrant == 3)
+    -cos_taylor(aradi) # isneg ? -cos_taylor(aradi) : -cos_taylor(aradi)
+  end
+end
+
+function sin(radian::DD)
+  if abs(radian) <= dd_pi_over_4
+      sin_taylor(radian)
+   elseif abs(radian) < dd_twopi
+      sinInCircle(radian)
+   else
+      r = mod2piAsTD(radian)
+      ddr = DD(r.hi,r.md)
+      sinInCircle(ddr)*cos(r.lo)+sin(r.lo)*cos(ddr)
+    end
+end
+
+#=
+  for radian in [0,8*pi)
+      relerr precise sin(radian) ~1.2e-32 (106 bits)
+   for radian in [0,4096^3 *pi)
+      relerr precise sin(radian) ~2e-32 (105 bits)
+   for radian > 4096^3 *pi
+      relerr 1.5e-29 (95 bits)
+=#
+function cosInCircle(radian::DD)
+  quadrant, radi = resolveQuadrantInCircle(radian)
+  isneg, aradi = signbit(radi), abs(radi)
+  if (quadrant == 0)
+    cos_taylor(aradi)
+  elseif (quadrant == 1)
+    isneg ? sin_taylor(aradi) : -sin_taylor(aradi)
+  elseif (quadrant == 2)
+    -cos_taylor(aradi)
+  else #if (quadrant == 3)
+    isneg ? -sin_taylor(aradi) : sin_taylor(aradi)
+  end
+end
+
+function cos(radian::DD)
+   if abs(radian) <= dd_pi_over_4
+      cos_taylor(radian)
+   elseif abs(radian < dd_twopi)
+      cosInCircle(radian)
+   else
+      r = mod2piAsTD(radian)
+      ddr = DD(r.hi,r.md)
+      cosInCircle(ddr)*cos(r.lo)-sinInCircle(ddr)*sin(r.lo)
+    end
+end
+
+
+# good for radian in 0..2pi(quadrants 0,1, 1/2 of q3)
+#=
+   for radian in [0,2*pi)
+      relerr tan(radian) ~ 5e-3 (106 bits)
+=#
+function tanInCircle(radian::DD)
+  quadrant, radi = resolveQuadrantInCircle(radian)
+  isneg, aradi = signbit(radi), abs(radi)
+  t = tan_taylor(aradi)
+  if (quadrant == 0)
+    isneg ? -t : t
+  elseif (quadrant == 1)
+    isneg ? recip(t) : -recip(t)
+  elseif (quadrant == 2)
+    isneg ? -t : t
+  else #if (quadrant == 3)
+    isneg ? recip(t) : -recip(t)
+  end
+end
+
+#=
+  for radian in [0,7*pi)
+      relerr precise tan(radian) ~3.3e-32 (104 bits)
+   for radian in [0,4096^3 *pi)
+      relerr precise sin(radian) ~5.5e-32 (103 bits)
+   for radian > 4096^3 *pi
+      relerr 8e-29 (93 bits)
+=#
+function tan(radian::DD)
+   if abs(radian.hi) < 9.0/64.0
+      tan_taylor(radian)
+   elseif abs(radian < dd_twopi)
+      tanInCircle(radian)
+   else
+      r = mod2piAsTD(radian)
+      ddr = DD(r.hi,r.md)
+      tddr = tanInCircle(ddr)
+      t = tan(r.lo)
+      (tddr+t) / (1.0-tddr*t)
+    end
 end
