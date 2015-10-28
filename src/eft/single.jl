d914fce5daa736c43874db175ec94e5dbce8cdd4
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2015-10-28 16:27:29 -0400

    Update single.jl

--- src/eft/single.jl
+++ src/eft/single.jl
@@ -1,10 +1,10 @@
-@inline function eftSquare(a::Float64)
+@inline function eftSquare(a::FloatingPoint)
     x = a * a
     y = fma(a, a, -x)
     x,y
 end
 
-function eftCube(a::Float64)
+function eftCube(a::FloatingPoint)
     p = a*a; e = fma(a, a, -p)
     x = p*a; p = fma(p, a, -x)
     y = e*a
@@ -15,7 +15,7 @@ end
 # presently 'y' must be negated to get the right result
 # I do not know why.
 #
-@inline function eftRecip(a::Float64)
+@inline function eftRecip(a::FloatingPoint)
      x = one(Float64)/a
      y = fma(x,a,-1.0)/a
      x,-y
