c45f8cecd3087e7105bd06a14321a71899d8beb4
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2015-10-27 23:27:04 -0400

    Update FLOAT125.jl

--- src/FLOAT125.jl
+++ src/FLOAT125.jl
@@ -49,8 +49,8 @@ include("arith/DDmanip.jl")
 include("arith/TDmanip.jl")
 
 # integer powers, reciprocal roots
-include("pow/DD.jl")
-include("pow/TD.jl")
+# include("pow/DD.jl")
+# include("pow/TD.jl")
 
 # log, exp
 include("exp/DD.jl")
