ecacce1c3898b86e51718c3406376a871e757292
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2015-10-28 02:15:24 -0400

    Update TD.jl

--- src/trigh/TD.jl
+++ src/trigh/TD.jl
@@ -1,21 +1,21 @@
 
 function sinh(x::TD)
   epx = exp(x)
-  emx = exp(-x)
+  emx = 1.0/epx
   epx = epx - emx
   divby2(epx)
 end
 
 function cosh(x::TD)
   epx = exp(x)
-  emx = exp(-x)
+  emx = 1.0/epx
   epx = epx + emx
   divby2(epx)
 end
 
 function tanh(x::TD)
   epx = exp(x)
-  emx = exp(-x)
+  emx = 1.0/epx
   n = (epx - emx)
   d = (epx + emx)
   n/d
