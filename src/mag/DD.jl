a9c570482e3951196502d6e6aa30003cf6c86588
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2015-10-28 00:26:53 -0400

    Update DD.jl

--- src/mag/DD.jl
+++ src/mag/DD.jl
@@ -1,3 +1,26 @@
+@inline function isless(a::DD, b::DD)
+    (a.hi < b.hi) || (a.hi==b.hi && a.lo<b.lo)
+end
+@inline function isless(a::DD, b::Float64)
+    (a.hi < b) || (a.hi==b && a.lo<zero(Float64))
+end
+@inline function isless(a::Float64, b::DD)
+    (a < b.hi) || (a==b.hi && b.lo<zero(Float64))
+end
+@inline function isequal(a::DD, b::DD)
+    (a.hi == b.hi) && (a.lo == b.lo)
+end
+@inline function isequal(a::DD, b::Float64)
+    (a.hi == b) && (a.lo == zero(Float64))
+end
+@inline function isequal(a::Float64, b::DD)
+    (a == b.hi) && (b.lo == zero(Float64))
+end
+
+
+
+
+
 function (floor)(a::DD)
     hi = floor(a.hi)
     lo = 0.0
