c4be9810c8123ed9206ff99df449381747769cef
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2015-10-28 16:57:35 -0400

    Update domainCheck.jl

--- src/util/domainCheck.jl
+++ src/util/domainCheck.jl
@@ -1,39 +1,49 @@
-@inline function isBetween{T<:MachineFloat}(value::T, lowerBound::T, upperBound::T)
-    lowerBound < value < upperBound
+immutable BotTop{T <: Real}
+    bot::T
+    top::T
+    
+    function BotTop(bot::T, top::T)
+        bot, top = min(bot,top), max(bot,top)
+        new(bot, top)
+    end
+end    
+
+@inline function isBetween{T<:Real}(value::T, bounds::BotTop{T})
+    bounds.bot < value < bounds.top
 end
-@inline function isNotBetween{T<:MachineFloat}(value::T, lowerBound::T, upperBound::T)
-    !isBetween(value,lowerBound,upperBound)
+@inline function isNotBetween{T<:Real}(value::T, bounds::BotTop{T})
+    !isBetween(value,bounds)
 end
-@inline function isAmoung{T<:MachineFloat}(value::T, lowerBound::T, upperBound::T)
-    lowerBound <= value <= upperBound
+@inline function isAmoung{T<:Real}(value::T, bounds::BotTop{T})
+    bounds.bot <= value <= bounds.top
 end
-@inline function isNotAmoung{T<:MachineFloat}(value::T, lowerBound::T, upperBound::T)
-    !isAmoung(value,lowerBound,upperBound)
+@inline function isNotAmoung{T<:Real}(value::T, bounds::BotTop{T})
+    !isAmoung(value,bounds)
 end
 
-@inline function isInClosedOpen{T<:MachineFloat}(value::T, lowerBound::T, upperBound::T)
-    lowerBound <= value <=upperBound
+@inline function isInClosedOpen{T<:Real}(value::T, bounds::BotTop{T})
+    bounds.bot <= value < bounds.top
 end
-@inline function isNotInClosedOpen{T<:MachineFloat}(value::T, lowerBound::T, upperBound::T)
+@inline function isNotInClosedOpen{T<:Real}(value::T, bounds::BotTop{T})
     !isInClosedOpen(value,lowerBound,upperBound)
 end
-@inline function isInOpenClosed{T<:MachineFloat}(value::T, lowerBound::T, upperBound::T)
-    lowerBound <= value <=upperBound
+@inline function isInOpenClosed{T<:Real}(value::T, bounds::BotTop{T})
+    bounds.bot < value <= bounds.top
 end
-@inline function isNotInOpClosed{T<:MachineFloat}(value::T, lowerBound::T, upperBound::T)
+@inline function isNotInOpenClosed{T<:Real}(value::T, bounds::BotTop{T})
     !isInOpenClosed(value,lowerBound,upperBound)
 end
 
-@inline function isAbove{T<:MachineFloat}(value::T, lowerBound::T)
-    lowerBound < value
+@inline function isAbove{T<:Real}(value::T, bounds::BotTop{T})
+    bounds.bot < value
 end
-@inline function isNotAbove{T<:MachineFloat}(value::T, lowerBound::T)
+@inline function isNotAbove{T<:Real}(value::T, bounds::BotTop{T})
     !isAbove(value,lowerBound)
 end
-      @inline function isBelow{T<:MachineFloat}(value::T, upperBound::T)
-    value < upperBound
+@inline function isBelow{T<:Real}(value::T, bounds::BotTop{T})
+    value < bounds.top
 end
-@inline function isNotBelow{T<:MachineFloat}(value::T, upperBound::T)
+@inline function isNotBelow{T<:Real}(value::T, bounds::BotTop{T})
     !isBelow(value,upperBound)
 end
 
