a874e7b36072a200d967bfc278dc34030c6e6a48
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2015-11-01 21:30:19 -0500

    Update timeit.jl

--- test/util/timeit.jl
+++ test/util/timeit.jl
@@ -1,16 +1,15 @@
 
 function timeit(fn::Function, arg::Real, n::Int)
-   mn = 9999999.9; mx=md=0.0;
+   mn = 9999999.9; md=0.0;
    a=fn(arg)
    for i in 1:n
       a = time_ns()
       b = fn(arg)
       c = time_ns()-a
       if (c < mn) mn = c end;
-      if (c > mx) mx = c end;
       md = md+c;
    end
    md1 = div(md,n)
-   Int(mn),Int(md1),Int(mx)
+   Int(mn),Int(md1),div(6*Int(mn)+Int(md1),7)
 end
 
